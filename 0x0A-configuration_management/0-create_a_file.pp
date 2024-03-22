6630cda771949bb09009129f9b702e25992a1c38
NkamoN <r.nkamo@gmail.com>
2024-03-22 16:47:13 +0200

    0. Create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,9 @@
 # Create a file in /tmp
 
-file { 'ALX':
+file { '/tmp/school':
   ensure  => 'present',
   content => 'I love Puppet',
   group   => 'www-data',
   mode    => '0744',
-  owner   => 'www-data',
-  path    => '/tmp/ALX',
+  owner   => 'www-data',,
 }
